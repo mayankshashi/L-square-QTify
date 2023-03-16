99fe4b9a016f4f5eb39b830cffb63a87d294419f
Mayank Shashi <mail2mayankshashi@gmail.com>
2023-03-16 12:24:52 +0530

    commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Card
+        image="https://png.pngtree.com/png-clipart/20210829/original/pngtree-music-album-cover-modern-style-color-sns-png-image_6664686.jpg"
+        follows={100}
+        title={"Abstract"}
+      />
     </>
   );
 }
